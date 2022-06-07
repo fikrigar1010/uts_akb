fe672b884550fe4b3b938e5b540c1f29d2c63ac5
fikrigar1010 <geroniobah@gmail.com>
2022-06-07 22:43:46 +0700

    first commit

---
+++ app/src/main/java/com/example/uts_10119106_ahmadfikri/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.example.uts_10119106_ahmadfikri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
